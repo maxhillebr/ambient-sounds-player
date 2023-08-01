df9b166f0bb882d6d96d30fa5d1e19eda0d7c80a
Max Hillebrand <max.hillebrand@yahoo.de>
2023-08-01 14:33:03 +0200

    Profile Image

--- src/components/headernav.js
+++ src/components/headernav.js
@@ -5,22 +5,32 @@ import "../css/headernav.css";
 
 const HeaderNav = () => {
   return (
-    <div className="header box">
-      <div className="logo">
-        {" "}
-        <a href="#default" className="logo">
-          <FontAwesomeIcon icon={faHeadphones} />
-        </a>
-      </div>
+    <>
+      <div className="header box">
+        <div className="logo">
+          {" "}
+          <a href="#default" className="logo">
+            <FontAwesomeIcon icon={faHeadphones} />
+          </a>
+        </div>
 
-      <div className="header-right">
-        <a className="active" href="#home">
-          Home
-        </a>
-        <a hrefName="#contact">Contact</a>
-        <a hrefName="#about">About</a>
+        <div className="header-right">
+          <div className="profile-img">
+            <img
+              src="../images/profile-pic.jpg"
+              alt="profile image"
+              width="50px"
+              height="auto"
+            />
+          </div>
+          <a className="active" href="#home">
+            Home
+          </a>
+          <a hrefName="#contact">Contact</a>
+          <a hrefName="#about">About</a>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
